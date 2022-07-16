61290e861041be9a63fbe6e91c5abcc47ce86178
simiyusarah1 <106378785+simiyusarah1@users.noreply.github.com>
2022-07-17 00:59:59 +0300

    Add files via upload

---
+++ Quadratic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    employee name[];
+    int employ num;
+    int KRA pin;
+    int hours worked;
+    int i;for(i=0;i<3,i++){
+        printf("Enter emplyees name:");
+        scanf("%s",name);
+        printf("Enter employees nummber:");
+        scanf("%d",employ_num);
+        printf("Enter employees KRA pin:");
+        scanf("%d,&KRA_pin");
+        printf("Enter employees hours worked:");
+        scanf("%d",&hours worked:);
+        double normal_pay=(1000*hours_worked);
+        double overtime_pay=(1.5*1000*(hours_worked-40));
+        double tax=0.3*(normal_pay-overtime_pay);
+        double net_income+(normal_pay+overtime_pay)=tax;
+        printf("Normal_pay:%lf\n",normal_pay);
+        printf(overtime)
+
+
+    }
+}
